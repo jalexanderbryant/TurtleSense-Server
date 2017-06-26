9b8608df1edd9bcf0f7b9254da3c41aebc6fb6d5
James Alexander <j.alexanderbryant@knights.ucf.edu>
2017-06-25 23:56:41 -0400

    Fix error in init.py

--- .ts_files/init.py
+++ .ts_files/init.py
@@ -2,7 +2,7 @@
 import requests
 
 print('hello world')
-api_endpoint = 'http://test-env.vdxd9zppmk.us-west-2.elasticbeanstalk.com/create_device' 
+api_endpoint = 'http://test-env.vdxd9zppmk.us-west-2.elasticbeanstalk.com/api/devices/create_device' 
 
 # Get device name from user and clean it up
 device_name = None
@@ -23,16 +23,18 @@ if serial_number is not None:
 print(f"Attempting to create device instance on server with device name '{device_name}' and serial number '{serial_number}'")
 
 # Send request to server to create the device
-payload = {'deviceName': device_name, 'serialNumber': serial_number}
-print("Issuing request...")
-response = requests.post(api_endpoint, data=payload)
+payload = { 'deviceName': device_name, 
+            'serialNumber': serial_number}
 
-print(response.text)
-response_json = response.json()
+print("Issuing request...")
+
+response = requests.post(api_endpoint, data=payload).json()
+
+print(response)
 
 # Setup files...
 with open('device-certificate.pem.crt', 'w') as f:
-   f.write(response_json["certs"]["certificate_pem"])
+   f.write(response["certs"]["certificate_pem"])
 
 with open('device-private.pem.key', 'w') as f: 
     f.write(response_json['certs']['private_key'])
