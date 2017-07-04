782c2effcdfd51404fe9d715153dd8dc2f208ce4
James Bryant <j.alexanderbryant@knights.ucf.edu>
2017-07-04 18:00:41 -0400

    Add CORS support.

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -8,7 +8,13 @@ module.exports = function(app){
   app.use(morgan('dev')); // ???
   app.use(bodyParser.urlencoded({ extended: true })); //???
   app.use(bodyParser.json()); // ???
-
   
+  // We need this for Cross Orgin Request Sharing
+  // Allow requests from anywhere
+  app.use(function(request, result, next){
+    result.setHeader('Access-Control-Allow-Origin', '*');
+    result.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+  }) 
 
 };
