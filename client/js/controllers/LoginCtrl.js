fdb19918c4b9c0e9e7e7483b91eb1dac24134f5c
James Bryant <j.alexanderbryant@knights.ucf.edu>
2017-07-04 16:50:56 -0400

    User registration and login basic pages.

--- client/js/controllers/LoginCtrl.js
+++ client/js/controllers/LoginCtrl.js
@@ -1,3 +1,12 @@
-angular.module('LoginCtrl', []).controller('LoginController', function($scope){
+angular.module('LoginCtrl', []).controller('LoginController', ['$scope', '$resource', function($scope, $resource){
   $scope.tagline = "Login to TurtleSense";
-});
+
+  $scope.login = function(user){
+    var payload = {
+        username 
+    }
+    var username = user.username;
+    var password = user.password;
+
+  }
+}]);
